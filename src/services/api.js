22f851237ebe7845f78a05656ceea3885bc9f9a7
lvieira268 <lvieira268@gmail.com>
2022-01-11 10:14:23 -0300

    atualiza chave de gh api
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'https://api.hgbrasil.com/finance?format=json-cors&key=72ee0ee9',
+	baseURL: 'https://api.hgbrasil.com/finance?format=json-cors&key=a3d1f35a',
 });
 
 export default api;
