64c3d9cc8ac9843cad095e147c7f58f87823bcfb
lvieira268 <lvieira268@gmail.com>
2022-01-11 10:01:15 -0300

    update configuração de service api
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'https://api.hgbrasil.com/finance?format=json-cors&key=bac1823f',
+	baseURL: 'https://api.hgbrasil.com/finance?format=json-cors&key=72ee0ee9',
 });
 
 export default api;
